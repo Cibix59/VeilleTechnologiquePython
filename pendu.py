1b64eb00704f49f04052f5fb72f9a18b35c9b195
Cibix59 <99341714+Cibix59@users.noreply.github.com>
2022-05-01 17:20:38 -0400

    feat : detection bonne lettre

--- pendu.py
+++ pendu.py
@@ -1,12 +1,20 @@
-
 import random
 
 
+enJeu = true
 
 file = open('mots.txt')
 f = file.readlines()
 i = random.randrange(0, len(f) - 1)
 
+mot = f[i][:-1]
 
+print(mot)
 
-print(f[i][:-1])
+while enJeu:
+    lettreChoisie = str(input())
+
+    if lettreChoisie.lower() not in mot.lower():
+        print("mauvaise lettre") 
+    else:
+        print("Bonne lettre")
