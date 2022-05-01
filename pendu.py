54490b5a8e0b269d3b85bee19bceff96c59c78e3
Cibix59 <99341714+Cibix59@users.noreply.github.com>
2022-05-01 17:52:15 -0400

    feat : version console fonctionnelle

--- pendu.py
+++ pendu.py
@@ -1,7 +1,27 @@
+from pickle import TRUE
 import random
 
 
-enJeu = true
+enJeu = TRUE
+motDevine = []
+vie = 5
+
+#genere la visualisation du mot actuel
+def getMotActuel(mot, motDevine=[]):
+    motActuelTMP = ''
+    lettreTrouvees = motDevine
+    for x in range(len(mot)):
+        if mot[x] != ' ':
+            motActuelTMP += '_ '
+            for i in range(len(lettreTrouvees)):
+                print("mo  : "+motDevine[i])
+                if mot[x] == lettreTrouvees[i]:
+                    motActuelTMP = motActuelTMP[:-2]
+                    motActuelTMP += mot[x].upper() + ' '
+        elif mot[x] == ' ':
+            motActuelTMP += ' '
+    return motActuelTMP
+
 
 file = open('mots.txt')
 f = file.readlines()
@@ -14,7 +34,30 @@ print(mot)
 while enJeu:
     lettreChoisie = str(input())
 
+    motDevine.append(lettreChoisie)
+
+
     if lettreChoisie.lower() not in mot.lower():
         print("mauvaise lettre") 
+        vie = vie -1
     else:
         print("Bonne lettre")
+        motActuel = getMotActuel(mot, motDevine)
+        print(motActuel)
+        if motActuel.count('_') == 0:
+            print("bien Joué !")
+            quit()
+
+    print("vie : "+ str(vie))
+    if vie == 0:
+        print("Game Over")
+        quit()
+
+
+
+
+
+                
+
+
+
