95f3315bf4ee2fac2a8a5b6769e18c702f59e203
Cibix59 <99341714+Cibix59@users.noreply.github.com>
2022-05-07 18:01:20 -0400

    feat : prototype interface PyGui

--- test2.py
+++ test2.py
@@ -3,11 +3,16 @@ from wsgiref import headers
 from requests_html import HTMLSession
 
 
+import dearpygui.dearpygui as dpg
+
+
+
+
 session = HTMLSession()
 
-query ='riviere+du+loup'
+lieu ='riviere+du+loup'
 #f permet d'y ajouter un parametre
-url =f'https://www.google.com/search?q=meteo+{query}'
+url =f'https://www.google.com/search?q=meteo+{lieu}'
 r = session.get(url,headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'})
 
 
@@ -36,3 +41,16 @@ print(prochainsJours)
 
 
 
+dpg.create_context()
+dpg.create_viewport(title='Custom Title', width=600, height=300)
+
+with dpg.window(label="Ma météo :"):
+    dpg.add_text("Metéo actuelle à "+lieu+" : "+temperature+unite+" \nHumidité à "+humidité+"")
+"""     dpg.add_button(label="bouton")
+    dpg.add_input_text(label="input", default_value="valeur par defaut")
+    dpg.add_slider_float(label="Slider", default_value=0.5, max_value=1) """
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
